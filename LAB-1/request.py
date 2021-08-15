3e0f3aad44c2dd2ea20bbe16e3192270ad8cfd89
arthurgbranco <agbranco@sga.pucminas.br>
2021-08-15 20:16:25 -0300

    better printing

--- LAB-1/request.py
+++ LAB-1/request.py
@@ -5,13 +5,6 @@ import json
 token = os.getenv('GITHUB_TOKEN')
 headers = {"Authorization": f"token {token}"}
 
-def run_query(query):
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-  
 query = """
 query example {
   search(query: "stars:>1", type: REPOSITORY, last: 100) {
@@ -26,5 +19,12 @@ query example {
 }
 """
 
-result = run_query(query)# Execute the query
+def run_query(query):
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception(f"Query failed to run by returning code of {request.status_code}. {query}")
+
+result = run_query(query)['data']['search']['nodes']
 print(json.dumps(result, indent=4, sort_keys=True))# Prettify result
