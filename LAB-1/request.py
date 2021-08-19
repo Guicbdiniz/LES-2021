bc0b5ef7c0ff50471846d558e91aaef47e7daa98
arthurgbranco <agbranco@sga.pucminas.br>
2021-08-19 10:39:20 -0300

    missing data on query

--- LAB-1/request.py
+++ LAB-1/request.py
@@ -6,16 +6,35 @@ token = os.getenv('GITHUB_TOKEN')
 headers = {"Authorization": f"token {token}"}
 
 query = """
-query example {
-  search(query: "stars:>1", type: REPOSITORY, last: 100) {
-    nodes {
-      ... on Repository {
-        nameWithOwner
-        createdAt
-        stargazerCount
+query{
+	search(query:"stars:>100", type:REPOSITORY, first:100){
+		nodes{
+		...on Repository{
+			nameWithOwner
+			url
+			stargazers{
+        totalCount
       }
-    }
-  }
+			createdAt
+			pullRequests(states: MERGED){
+				totalCount
+			}
+			releases(first:1){
+				totalCount
+			}
+			updatedAt
+			primaryLanguage{
+				name
+			}
+			open: issues(states:OPEN){
+				totalCount
+			}
+			closed: issues(states:CLOSED){
+				totalCount
+			}
+		}
+		}
+	}
 }
 """
 
@@ -26,7 +45,7 @@ def run_query(query):
     else:
         raise Exception(f"Query failed to run by returning code of {request.status_code}. {query}")
 
-result = run_query(query)['data']['search']['nodes']
+result = run_query(query)
 print(json.dumps(result, indent=4, sort_keys=True))# Prettify result
 
 print(f"\nResults count: {len(result)}")
