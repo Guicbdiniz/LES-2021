1cc13ffa52892cbc0513a1ae7c7ecb80631d25ac
arthurgbranco <agbranco@sga.pucminas.br>
2021-08-23 12:34:40 -0300

    1000 results + added warning to ended pages

--- LAB-1/request.py
+++ LAB-1/request.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import pandas as pd
+import json
 
 token = os.getenv('GITHUB_TOKEN')
 
@@ -8,7 +9,7 @@ def run_query(cursor):
     headers = {"Authorization": f"token {token}"}
     query = '''
     query{
-        search(query:"stars:>100", type:REPOSITORY, first:100, after:"''' + cursor + '''"){
+        search(query:"stars:>0", type:REPOSITORY, first:100, after:"''' + cursor + '''"){
         pageInfo{
             hasNextPage
             endCursor
@@ -53,16 +54,18 @@ def paginate(pages):
     cursor="Y3Vyc29yOjAK" # Page = 0
     csv = []
 
-    while (has_next_page & (pages > 0)):
+    while (has_next_page and (pages > 0)):
         data = run_query(cursor)['data']
         pages = pages - 1
         has_next_page = data['search']['pageInfo']['hasNextPage']
         cursor = data['search']['pageInfo']['endCursor']
         result = data['search']['nodes']
         # print(json.dumps(result, indent=4, sort_keys=True))# Prettify result # Debug
-        csv.append(result)
-
+        csv.append(result)  
+        if not has_next_page:
+            print("has_next_page returned false")
+       
     return csv
 
-results = paginate(100)
+results = paginate(10)
 pd.DataFrame(results).to_csv('results.csv')
