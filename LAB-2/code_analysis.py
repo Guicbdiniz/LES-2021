1dcc6c3c06704bc17eb71fc296de7bc1486f8b7f
Guicbdiniz <guicbdeniz@hotmail.com>
2021-09-08 22:41:42 -0300

    add second argument to script

--- LAB-2/code_analysis.py
+++ LAB-2/code_analysis.py
@@ -9,21 +9,27 @@ logger = logging.getLogger(__name__)
 def main():
     try:
         current_repo_index = int(sys.argv[1])
+        number_of_repos_to_be_analyzed = int(sys.argv[2])
 
-        logger.info(f'Current repository index: {current_repo_index}')
+        for _ in range(number_of_repos_to_be_analyzed):
+            logger.info(f'Current repository index: {current_repo_index}')
 
-        data_manager = RepositoriesDataManager()
-        data_manager.analyse_repository(current_repo_index)
-        data_manager.save_data()
-        data_manager.clean_up()
+            data_manager = RepositoriesDataManager()
+            data_manager.analyse_repository(current_repo_index)
+            data_manager.save_data()
+            data_manager.clean_up()
+            if current_repo_index == 999:
+                break
+            current_repo_index += 1
+        logger.info('Closing up')
     except ValueError:
         logger.error('Incorrect arguments types.')
         logger.error(
-            'Correct usage: python code_analysis {current_repo_index: int}.')
+            'Correct usage: python code_analysis {current_repo_index: int} {number_of_repos_to_be_analysed: int}.')
     except IndexError:
         logger.error('Incorrect number of arguments.')
         logger.error(
-            'Correct usage: python code_analysis {current_repo_index: int}.')
+            'Correct usage: python code_analysis {current_repo_index: int} {number_of_repos_to_be_analysed: int}.')
     except FileNotFoundError:
         logger.error('API results csv file not found. Run the github_fetch.py module first.')
     except Exception as e:
