43584afd80b549e2c1d2af2194fbfd1989cc301b
Guicbdiniz <guicbdeniz@hotmail.com>
2021-10-13 22:31:34 -0300

    fastapi + main.py

---
+++ LAB-3/src/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from simpletransformers.classification import ClassificationModel
+
+
+class Issue(BaseModel):
+    title: str
+
+
+app = FastAPI()
+
+
+@app.post("/")
+async def root(issue: Issue):
+    try:
+        issue_title = issue.title
+        if issue_title is None:
+            raise Exception('Invalid request body')
+
+        model = ClassificationModel("roberta", "../outputs/checkpoint-1313-epoch-1", use_cuda=False)
+
+        prediction, _ = model.predict([issue_title])
+        print(prediction)
+
+        return {'type': 'bug'}
+    except Exception as e:
+        print(f'Error: {e}')
+        return {'type': 'invalid'}
